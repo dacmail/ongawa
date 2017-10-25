e957f0967ba1d1063ab7b333481902a5aea8d102
Daniel Aguilar <dacmail@gmail.com>
2017-10-25 10:35:20 +0200

    Responsive blog

--- wp-content/themes/ongawa2017/single.php
+++ wp-content/themes/ongawa2017/single.php
@@ -1,18 +1,5 @@
 <section class="blog-wrapper container">
-  <section class="blog-list">
-    <?php if (!have_posts()) : ?>
-      <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'sage'); ?>
-      </div>
-      <?php get_search_form(); ?>
-    <?php endif; ?>
-
-    <?php while (have_posts()) : the_post(); ?>
-      <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-    <?php endwhile; ?>
-
-    <?php the_posts_navigation(); ?>
-  </section>
+  <?php get_template_part('templates/content', 'single'); ?>
   <aside class="sidebar">
     <?php get_template_part('templates/sidebar'); ?>
   </aside>
--- wp-content/themes/ongawa2017/templates/content-single.php
+++ wp-content/themes/ongawa2017/templates/content-single.php
@@ -1,15 +1,31 @@
+<?php use Roots\Sage\Extras; ?>
+
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
+    <div class="post__meta">
+      <div class="post__meta__categories"><?php the_category(' ', ' '); ?></div>
+      <div class="post__meta__share">
+        <a target="_blank" href="https://twitter.com/home?status=<?= urlencode(get_the_title()) . ' ' . urlencode(get_permalink()); ?>" class="post__meta__share__twitter"><?= Extras\ungrynerd_svg('icon-twitter'); ?></a>
+        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode(get_permalink()); ?>" class="post__meta__share__facebook"><?= Extras\ungrynerd_svg('icon-facebook'); ?></a>
+        <a target="_blank" href="https://api.whatsapp.com/send?text=<?= get_the_title() . ' ' . urlencode(get_permalink()); ?>" class="post__meta__share__whatsapp"><?= Extras\ungrynerd_svg('icon-whatsapp'); ?></a>
+      </div>
+    </div>
+    <?php the_post_thumbnail('slide', array('class' => 'post__thumb')) ?>
+    <h1 class="post__title"><?php the_title(); ?></h1>
+    <div class="post__meta post__meta--author">
+      <p class="post__meta__by"><a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author"><?= get_the_author(); ?></a></p>
+      <time class="post__meta__date"><?php the_time(get_option('date_format')); ?></time>
+    </div>
+    <div class="post__content">
       <?php the_content(); ?>
     </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <div class="post__meta post__meta--footer">
+      <div class="post__meta__tags"><?php the_tags(' ', ' '); ?></div>
+      <div class="post__meta__share">
+        <a target="_blank" href="https://twitter.com/home?status=<?= urlencode(get_the_title()) . ' ' . urlencode(get_permalink()); ?>" class="post__meta__share__twitter"><?= Extras\ungrynerd_svg('icon-twitter'); ?></a>
+        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode(get_permalink()); ?>" class="post__meta__share__facebook"><?= Extras\ungrynerd_svg('icon-facebook'); ?></a>
+        <a target="_blank" href="https://api.whatsapp.com/send?text=<?= get_the_title() . ' ' . urlencode(get_permalink()); ?>" class="post__meta__share__whatsapp"><?= Extras\ungrynerd_svg('icon-whatsapp'); ?></a>
+      </div>
+    </div>
   </article>
 <?php endwhile; ?>
