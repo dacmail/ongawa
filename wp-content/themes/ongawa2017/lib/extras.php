c26e8c68d1eb55f4c81a73cc7caff5aa1cdaa54c
Daniel Aguilar <dacmail@gmail.com>
2017-11-20 15:40:31 +0100

    Paises

--- wp-content/themes/ongawa2017/lib/extras.php
+++ wp-content/themes/ongawa2017/lib/extras.php
@@ -111,3 +111,40 @@ function ungrynerd_doc_taxonomies() {
     );
 }
 add_action( 'init', __NAMESPACE__ . '\ungrynerd_doc_taxonomies', 0);
+
+
+/* DOCUMENTS POST TYPE */
+add_action('init',  __NAMESPACE__ . '\ugnrynerd_country_post_type');
+function ugnrynerd_country_post_type()  {
+  $labels = array(
+    'name' => __('Paises', 'ungrynerd'),
+    'singular_name' => __('País', 'ungrynerd'),
+    'add_new' => __('Añadir País', 'ungrynerd'),
+    'add_new_item' => __('Añadir País', 'ungrynerd'),
+    'edit_item' => __('Editar País', 'ungrynerd'),
+    'new_item' => __('Nuevo País', 'ungrynerd'),
+    'view_item' => __('Ver Paises', 'ungrynerd'),
+    'search_items' => __('Buscar Paises', 'ungrynerd'),
+    'not_found' =>  __('No se han encontrado Paises ', 'ungrynerd'),
+    'not_found_in_trash' => __('No hay Paises en la papelera', 'ungrynerd'),
+    'parent_item_colon' => ''
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'query_var' => true,
+    'capability_type' => 'post',
+    'show_in_nav_menus' => false,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'menu_position' => 5,
+    'rewrite' => array( 'slug' => 'paises' ),
+    'taxonomies' => array(),
+    'has_archive' => true,
+    'supports' => array('title', 'editor', 'thumbnail')
+  );
+  register_post_type('un_country',$args);
+}
---
+++ wp-content/themes/ongawa2017/single-un_country.php
@@ -0,0 +1,51 @@
+<?php use Roots\Sage\Extras; ?>
+
+<section class="container">
+  <?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class('country'); ?>>
+      <?php if (has_post_thumbnail()): ?>
+        <?php the_post_thumbnail('full', array('class' => 'country__header')) ?>
+      <?php endif ?>
+      <div class="country__container">
+        <div class="country__content">
+          <?php the_content(); ?>
+        </div>
+        <aside class="country__aside">
+          <div class="country__aside__header">
+            <h1 class="country__aside__title"><?php the_title(); ?></h1>
+            <?php the_field('country_summary') ?>
+          </div>
+          <div class="country__aside__map">
+            <?php the_field('country_map'); ?>
+          </div>
+          <div class="country__aside__data">
+            <?php the_field('country_data'); ?>
+          </div>
+          <?php $button = get_field('country_link'); ?>
+          <?php if ($button) : ?>
+            <a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>" class="btn-simple country__aside__link"><?php echo $button['title']; ?></a>
+          <?php endif; ?>
+        </aside>
+      </div>
+
+      <?php if (have_rows('country_related')) : ?>
+      <section class="country-related">
+        <h2 class="section-title"><?php esc_html_e('Actualidad en', 'ungrynerd'); ?> <?php the_title(); ?></h2>
+        <div class="country-related__wrap">
+          <?php while (have_rows('country_related')): the_row(); ?>
+            <artcile class="country-related__item">
+              <?php $image = get_sub_field('country_related_img'); ?>
+              <a href="<?php the_sub_field('country_related_link'); ?>"><?php echo wp_get_attachment_image($image, 'square', false, array('class' => 'country-related__item__image')); ?></a>
+              <div class="country-related__item__wrap">
+                <p class="country-related__item__pretitle"><?php the_sub_field('country_related_pretitle'); ?></p>
+                <h2 class="country-related__item__title"><a href="<?php the_sub_field('country_related_link'); ?>"><?php the_sub_field('country_related_title'); ?></a></h2>
+                <a class="country-related__item__link" href="<?php the_sub_field('country_related_link'); ?>"><?= Extras\ungrynerd_svg('icon-arrow'); ?></a>
+              </div>
+            </artcile>
+          <?php endwhile; ?>
+        </div>
+      </section>
+    <?php endif; ?>
+    </article>
+  <?php endwhile; ?>
+</section>
